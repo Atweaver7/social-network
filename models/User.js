d64da2b35f2ff3e857aade2659a52d36fc50634c
Aaron Weaver <95551676+Atweaver7@users.noreply.github.com>
2022-04-22 11:28:53 -0700

    added basic file structure

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    }
+
+})
