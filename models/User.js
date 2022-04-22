a4a321b4e773ff1033d5c8cea7ca989539f762d6
Aaron Weaver <95551676+Atweaver7@users.noreply.github.com>
2022-04-22 11:52:40 -0700

    added server code and user schema

--- models/User.js
+++ models/User.js
@@ -13,6 +13,37 @@ const UserSchema = new Schema({
         required: true,
         unique: true,
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-    }
+    },
 
+});
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 12,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    userName: {
+        type: String,
+        required: true,
+    },
+    reactions: {reactionSchema}
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+reactionSchema.virtual('reactionCount').get(function() {
+    return this.replies.length;
 })
