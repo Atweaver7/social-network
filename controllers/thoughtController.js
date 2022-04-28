2b5882a21fdad948c4e7a2837971433f0736dd94
Aaron Weaver <95551676+Atweaver7@users.noreply.github.com>
2022-04-28 12:37:35 -0700

    added thought model

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,11 +2,24 @@ const { Thought, User } = require("../models")
 
 const thoughtController = {
 
+thoughtCreate({params, body}, res) {
+    Thought.create(body)
+    .then(({_id}) => {
+        return Users.findOneAndUpdate({ _id: params.userId}, {$push: {thoughts: _id}}, {new: true});
+    })
+    .then(dbThoughtsData => {
+        if(!dbThoughtsData) {
+            res.status(404).json({message: 'No thoughts with this particular ID!'});
+            return;
+        }
+        res.json(dbThoughtsData)
+    })
+    .catch(err => res.json(err)); 
+},
 
 
 
 
 
 
-    
 }
